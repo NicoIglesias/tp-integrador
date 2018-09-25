712a16c9f8b450b65abbf7db92a18186e073d12b
Rquintero97 <43192230+Rquintero97@users.noreply.github.com>
2018-09-25 19:06:12 -0300

    Add files via upload

---
+++ proyecto/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Algo Nuevo, Algo Viejo</title>
+    <link href="css/styles.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <!-- Header -->
+    <header class="main-header">
+      <div class="logo">
+        <img src="clases/Proyecto/logomin2.png" alt="logo Algo Nuevo Algo Viejo" class="main-logo">
+
+      </div>
+
+
+      <nav class="main-nav">
+        <a href="#">Novias</a>
+        <a href="#">Quinceaneras</a>
+        <div class="logo"</div>
+        <a href="#">Fiesta</a>
+        <a href="#">Madrinas</a>
+      </nav>  
+
+  </body>
+</html>
