ca2597b478a628435ee0dafded43900ef76c499d
Rquintero97 <r.quinttero@gmail.com>
2018-09-26 17:20:43 -0300

    logo fixed y boton registro funcional

--- proyecto/index.php
+++ proyecto/index.php
@@ -10,7 +10,7 @@
     <!-- Header -->
     <header class="main-header">
       <div class="logo">
-        <img src="clases/Proyecto/logomin2.png" alt="logo Algo Nuevo Algo Viejo" class="main-logo">
+        <img src="images/logomin2.png" alt="logo Algo Nuevo Algo Viejo" class="main-logo">
 
       </div>
 
@@ -21,7 +21,12 @@
         <div class="logo"</div>
         <a href="#">Fiesta</a>
         <a href="#">Madrinas</a>
-      </nav>  
+      </nav>
+      <nav class="interactive-nav">
+        <a href="#">Log in</a>
+        <a href="registro.php">Registrate</a>
+        <input type="text" placeholder="Buscar..">
+      </nav>
 
   </body>
 </html>
