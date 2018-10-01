a44d481764e19cbe2df01925038032ca54238a61
nicolas iglesias <nico.iglesias96@gmail.com>
2018-10-01 01:24:30 -0300

    aca agrego login y registro mejorcitos

---
+++ proyecto/login.php
@@ -0,0 +1,92 @@
+<?php
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title></title>
+  </head>
+  <style>
+    body{
+      background-color: #FCE9E9;
+      text-align: center;
+    }
+    form {
+      position: relative;
+      z-index: 1;
+      background: #FFFFFF;
+      max-width: 360px;
+      margin: 60px auto 100px;
+      padding: 45px;
+      text-align: center;
+      box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+    .form input {
+      font-family: "Roboto", sans-serif;
+      outline: 0;
+      background: #f2f2f2;
+      width: 100%;
+      border: 0;
+      margin: 0 0 15px;
+      padding: 15px;
+      box-sizing: border-box;
+      font-size: 14px;
+
+    }
+    .myButton {
+      -moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+      -webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+      box-shadow:inset 0px 1px 0px 0px #ffffff;
+      background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf));
+      background:-moz-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+      background:-webkit-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+      background:-o-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+      background:-ms-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+      background:linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
+      filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf',GradientType=0);
+      background-color:#ededed;
+      -moz-border-radius:6px;
+      -webkit-border-radius:6px;
+      border-radius:6px;
+      border:1px solid #dcdcdc;
+      display:inline-block;
+      cursor:pointer;
+      color:#777777;
+      font-family:Arial;
+      font-size:15px;
+      font-weight:bold;
+      padding:6px 24px;
+      text-decoration:none;
+      text-shadow:0px 1px 0px #ffffff;
+    }
+    .myButton:hover {
+      background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed));
+      background:-moz-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+      background:-webkit-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+      background:-o-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+      background:-ms-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+      background:linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
+      filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed',GradientType=0);
+      background-color:#dfdfdf;
+    }
+    .myButton:active {
+     position:relative;
+     top:1px;
+    }
+
+  </style>
+  <body>
+
+    <form class="form">
+      <h1>Bienvenido/a</h1> <br><br>
+      Usuario <br> <input type="text" name="usuario" placeholder="Ingrese usuario"> <br>
+      Contraseña  <br> <input type="password" name="" placeholder="Ingrese contraseña"> <br>
+      <input class="myButton" type="button" name="" value="Login"> <br> <br>
+      <a href="registro.php">¿No tienes cuenta? <br> <input class="myButton" type="button" name="" value="Crear"></a>
+    </form>
+  </body>
+</html>
--- proyecto/registro.php
+++ proyecto/registro.php
@@ -1,6 +1,6 @@
 <?php
 
-  $tituloRegistro = "Registrate Gratis";
+  $tituloRegistro = "Registrese";
 
 
  ?>
@@ -9,30 +9,95 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Registro</title>
-    <link rel="stylesheet" href="css/styles.css">
   </head>
+  <style media="screen">
+    body{
+      background-color: #BCDCE4;
+      text-align: center;
+    }
+    form {
+      position: relative;
+      z-index: 1;
+      background: #FFFFFF;
+      max-width: 360px;
+      margin: 60px auto 100px;
+      padding: 45px;
+      text-align: center;
+      box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+    .form input {
+      font-family: "Roboto", sans-serif;
+      outline: 0;
+      background: #f2f2f2;
+      width: 100%;
+      border: 0;
+      margin: 0 0 15px;
+      padding: 15px;
+      box-sizing: border-box;
+      font-size: 14px;
+      }
+
+    .myButton {
+	    -moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	    -webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	    box-shadow:inset 0px 1px 0px 0px #ffffff;
+	    background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf));
+	    background:-moz-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	    background:-webkit-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	    background:-o-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	    background:-ms-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	    background:linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
+	    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf',GradientType=0);
+	    background-color:#ededed;
+	    -moz-border-radius:6px;
+	    -webkit-border-radius:6px;
+	    border-radius:6px;
+	    border:1px solid #dcdcdc;
+	    display:inline-block;
+	    cursor:pointer;
+	    color:#777777;
+	    font-family:Arial;
+	    font-size:15px;
+	    font-weight:bold;
+	    padding:6px 24px;
+	    text-decoration:none;
+	    text-shadow:0px 1px 0px #ffffff;
+    }
+    .myButton:hover {
+	    background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed));
+	    background:-moz-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	    background:-webkit-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	    background:-o-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	    background:-ms-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	    background:linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
+	    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed',GradientType=0);
+	    background-color:#dfdfdf;
+    }
+    .myButton:active {
+	   position:relative;
+	   top:1px;
+    }
+  </style>
   <body>
-    <h1><?php echo $tituloRegistro ?></h1>
-    <form class="" action="index.html" method="post">
+
+    <form class="form" action="index.html" method="post">
+      <h1><?php echo $tituloRegistro ?></h1> <br>
       Nombre completo: <br>
-      <input type="text" name="nombre completo" value="Ingrese nombre completo"> <br>
+      <input type="text" name="nombre completo" placeholder="Ingrese nombre completo"> <br>
       Nombre de Usuario: <br>
-      <input type="text" name="Username" value="Nombre de Usuario"> <br>
-      Pais de nacimiento: <br>
-      <select>
-        <option value="Argentina">Argentina</option>
-        <option value="Uruguay">Uruguay</option>
-      </select> <br>
+      <input type="text" name="Username" placeholder="Ingrese nombre de usuario"> <br>
       Correo electronico: <br>
-      <input type="email" name="email" value="email"> <br>
+      <input type="email" name="email" placeholder="Ingrese email"> <br>
       Contraseña: <br>
-      <input type="password" name="contraseña" value="contraseña"> <br>
+      <input type="password" name="contraseña" placeholder="Contraseña"> <br>
       Repita contraseña: <br>
-      <input type="password" name="contraseña" value="repita contraseña"> <br>
+      <input type="password" name="contraseña" placeholder="Repita contraseña"> <br>
       Imagen de perfil: <br>
-      <input type="file" name="" value=""> <br>
-      <input type="button" name="button" value="ENVIAR">
+      <input type="file" name="" value=""> <br><br>
+      <input class="myButton" type="submit" name="button" value="ENVIAR">
     </form>
   </body>
 </html>
