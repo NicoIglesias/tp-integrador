420482a07a4287eab73276ff0fbff8ebb71b6ab4
nicolas iglesias <nico.iglesias96@gmail.com>
2018-11-12 00:33:34 -0300

    solucion de err-undefined-mejoras en log y registr

--- proyecto/registro.php
+++ proyecto/registro.php
@@ -5,6 +5,7 @@
 
     $_POST['nombre_usuario'] = trim($_POST['nombre_usuario']);
     $_POST['contraseña'] = trim($_POST['contraseña']);
+    $_POST['confirmarContra'] = trim($_POST['confirmarContra']);
     $_POST['email'] = trim($_POST['email']);
 
     //validacion del nombre de usuario
@@ -26,6 +27,11 @@
       $errores['contraseña'] = "La contraseña debe tener al menos 6 caracteres";
     }
 
+    // confirmacion de contraseña
+
+    if ($_POST['confirmarContra'] !== $_POST['contraseña'] ) {
+      $errores['confirmarContra'] = "Las contraseñas no coinciden";
+    }
 
 
 
@@ -42,6 +48,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <title>Registro</title>
   </head>
   <style media="screen">
@@ -60,7 +67,7 @@
       box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
     }
     .form input {
-      font-family: "Roboto", sans-serif;
+      font-family: 'Josefin Sans', sans-serif;
       outline: 0;
       background: #f2f2f2;
       width: 100%;
@@ -71,46 +78,7 @@
       font-size: 14px;
       }
 
-    .myButton {
-	    -moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
-	    -webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
-	    box-shadow:inset 0px 1px 0px 0px #ffffff;
-	    background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf));
-	    background:-moz-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
-	    background:-webkit-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
-	    background:-o-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
-	    background:-ms-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
-	    background:linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
-	    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf',GradientType=0);
-	    background-color:#ededed;
-	    -moz-border-radius:6px;
-	    -webkit-border-radius:6px;
-	    border-radius:6px;
-	    border:1px solid #dcdcdc;
-	    display:inline-block;
-	    cursor:pointer;
-	    color:#777777;
-	    font-family:Arial;
-	    font-size:15px;
-	    font-weight:bold;
-	    padding:6px 24px;
-	    text-decoration:none;
-	    text-shadow:0px 1px 0px #ffffff;
-    }
-    .myButton:hover {
-	    background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed));
-	    background:-moz-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
-	    background:-webkit-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
-	    background:-o-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
-	    background:-ms-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
-	    background:linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
-	    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed',GradientType=0);
-	    background-color:#dfdfdf;
-    }
-    .myButton:active {
-	   position:relative;
-	   top:1px;
-    }
+
     .error{
       color: red;
       font-size: 10px;
@@ -125,7 +93,7 @@
       <input type="text" name="nombre completo" placeholder="Ingrese nombre completo"> <br>
       Nombre de Usuario: <br>
       <span class="error"><?php echo $errores['nombre_usuario']??''; ?></span> <br>
-      <input type="text" name="Username" placeholder="Ingrese nombre de usuario">
+      <input type="text" name="nombre_usuario" placeholder="Ingrese nombre de usuario">
       Correo electronico: <br>
       <span class="error"><?php echo $errores['email']??''; ?></span> <br>
       <input type="email" name="email" placeholder="Ingrese email"><br>
@@ -133,10 +101,13 @@
       <span class="error"><?php echo $errores['contraseña']??''; ?></span> <br>
       <input type="password" name="contraseña" placeholder="Contraseña"> <br>
       Repita contraseña: <br>
-      <input type="password" name="contraseña" placeholder="Repita contraseña"> <br>
-      Imagen de perfil: <br>
+      <span class="error"><?php echo $errores['confirmarContra']??''; ?></span> <br>
+      <input type="password" name="confirmarContra" placeholder="Repita su contraseña"> <br>
+      Imagen de perfil: <br><br>
       <input type="file" name="" value=""> <br><br>
-      <button type="submit">registrarse</button>
+      <input class="btn btn-primary btn btn-outline-success" type="submit" name="submit" value="Registrese">
+      <a class="btn btn-primary btn btn-outline-danger" href ="index.php">Cancelar</a>
     </form>
+
   </body>
 </html>
